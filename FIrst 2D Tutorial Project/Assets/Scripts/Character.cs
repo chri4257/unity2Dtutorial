944e9c772de00750321181c390e87b5d10b5f4ae
chri4257 <33205104+chri4257@users.noreply.github.com>
2018-12-30 15:49:26 +0000

    Seperated into charecter and player scripts

---
+++ FIrst 2D Tutorial Project/Assets/Scripts/Character.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// abstract class stops this charecter class being added to the game
+// can only add player, or enemy script to an object, but they inherit this class
+public abstract class Character : MonoBehaviour {
+
+	// Define the correct animator
+	// Protected means can access in any script whichi inherits this class
+	protected Animator myAnimator;
+
+	[SerializeField]
+	protected Transform knifePosition;
+
+	// Define Speed
+	// Private means can't access from other scripts
+	// Serialized will make it appear in Unity
+	[SerializeField]
+	protected float movementSpeed;
+
+	protected bool facingRight;
+
+	// Knife prefab
+	[SerializeField]
+	private GameObject knifePrefab;
+
+	public bool Attack { get; set; }
+	public bool Throw { get; set; }
+
+
+	// Use this for initialization
+	// virtual means it can be overwritten
+	public virtual void Start () {
+
+		// Initially facing right
+		facingRight = true;
+		// Get correct Animator
+		myAnimator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ChangeDirection()
+	{
+		facingRight = !facingRight;
+		transform.localScale = new Vector3(transform.localScale.x*-1, 1, 1);
+	}
+
+	public virtual void ThrowKnife(int value)
+	{
+		if (facingRight)
+		{
+			// Create an instance of knife prefab. At the correct knife position. Rotated in correct direction
+			// This instance is called tmp
+			GameObject tmp = (GameObject)Instantiate(knifePrefab, knifePosition.position, Quaternion.Euler(new Vector3(0, 0, -90)));
+			// We then look at the tmp instance and look at script Knife.cs and find the Intialize public function. We feed this Initialize a right Vector
+			tmp.GetComponent<Knife>().Initialize(Vector2.right);
+		}
+		else
+		{
+			GameObject tmp = (GameObject)Instantiate(knifePrefab, knifePosition.position , Quaternion.Euler(new Vector3(0, 0, 90)));
+			tmp.GetComponent<Knife>().Initialize(Vector2.left);
+		}
+	}
+
+}
--- FIrst 2D Tutorial Project/Assets/Scripts/Player.cs
+++ FIrst 2D Tutorial Project/Assets/Scripts/Player.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+
+// Player inherits from Charecter. Player is of type Charecter
+public class Player : Character {
 
 	private static Player instance;
 	// Some weird instance thing
@@ -19,19 +21,12 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	// Define the correct animator
-	private Animator myAnimator;
 
-	[SerializeField]
-	private Transform knifePosition;
 
-	// Define Speed
-	// Private means can't access from other scripts
-	// Serialized will make it appear in Unity
-	[SerializeField]
-	private float movementSpeed;
 
-	private bool facingRight;
+
+
+
 
 	// We select groundPoints from the Unity inspector
 	// These ground points will let us know if we are grounded
@@ -48,10 +43,6 @@ public class Player : MonoBehaviour {
 	[SerializeField]
 	private float jumpForce;
 
-	// Knife prefab
-	[SerializeField]
-	private GameObject knifePrefab;
-
 	// AirControl allows us to move whilst in the air
 	[SerializeField]
 	private bool airControl;
@@ -59,29 +50,32 @@ public class Player : MonoBehaviour {
 	// This defines we are using a Rigidbody
 	public Rigidbody2D MyRigidbody { get; set; }
 
-	public bool Attack { get; set; }
+
 	public bool Slide { get; set; }
 	public bool Jump { get; set; }
 	public bool OnGround { get; set; }
-	public bool Throw { get; set; }
+
+//	private Vector2 startPos;
 
 
 
 	// Use this for initialization
-	void Start () {
-		// Initially facing right
-		facingRight = true;
+	public override void Start () {
+		base.Start();
+
 		// This means that we set the rigidbody equal to the correct ones (i.e. the player that we run this script on) This basically gets the right reference for the correct rigidbody
 		MyRigidbody = GetComponent<Rigidbody2D>();
-		// Get correct Animator
-		myAnimator = GetComponent<Animator>();
+		// To define starting position
+	//	startPos = Transform.position;
+
 	}
+
 	// Update to check every single frame
 	void Update() {
 		// Check for HandleInputs
 		HandleInputs();
 	}
-	// Update is called once per frame
+
 	// We change from Update to fixedUpdate. Uses set timeframe rather than your computer's fps
 	void FixedUpdate () {
 			// This gets the input from controller to left or right and sets horizonal
@@ -97,9 +91,6 @@ public class Player : MonoBehaviour {
 			HandleLayers();
 			// Test if we need to Flip
 			Flip(horizontal);
-
-
-
 	}
 
 	// This handles inputs to player
@@ -149,19 +140,13 @@ public class Player : MonoBehaviour {
 		myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
 	}
 
-
 	// FLips the player from left to right
 	private void Flip(float horizontal)
 	{
 		// If moving right and not facing right then flip. Or if moving left and facing right flip
 		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
 		{
-			facingRight = !facingRight;
-			// We get the scale of the player
-			Vector3 theScale = transform.localScale;
-			// We flip the x scale
-			theScale.x = -theScale.x;
-			transform.localScale = theScale;
+			ChangeDirection();
 		}
 	}
 
@@ -204,25 +189,13 @@ public class Player : MonoBehaviour {
 	}
 
 	// Throw Knife
-	public void ThrowKnife(int value)
+	public override void ThrowKnife(int value)
 	{
 		// value 1 = in the air, value 0 = on the ground
 		// remember both animation layers run at the same time, so it would make two layers if we didn't stop it
 		if (!OnGround && value == 1 || OnGround && value == 0)
 		{
-			if (facingRight)
-			{
-				// Create an instance of knife prefab. At the correct knife position. Rotated in correct direction
-				// This instance is called tmp
-				GameObject tmp = (GameObject)Instantiate(knifePrefab, knifePosition.position, Quaternion.Euler(new Vector3(0, 0, -90)));
-			  // We then look at the tmp instance and look at script Knife.cs and find the Intialize public function. We feed this Initialize a right Vector
-				tmp.GetComponent<Knife>().Initialize(Vector2.right);
-			}
-			else
-			{
-				GameObject tmp = (GameObject)Instantiate(knifePrefab, knifePosition.position , Quaternion.Euler(new Vector3(0, 0, 90)));
-				tmp.GetComponent<Knife>().Initialize(Vector2.left);
-			}
+			base.ThrowKnife(value);
 		}
 
 	}
